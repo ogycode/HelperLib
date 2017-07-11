51b8d790121249103c8c4096be33d5a6235233df
ogycode <verlokav@gmail.com>
2017-07-11 22:20:36 +0300

    Complete Manager and Language for Localization part of lib.

--- src/HelperLib/Localization/Manager.cs
+++ src/HelperLib/Localization/Manager.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Verloka.HelperLib.Localization
@@ -7,29 +8,52 @@ namespace Verloka.HelperLib.Localization
     public class Manager
     {
         public event Action<Manager> LanguageChanged;
-        public event Action LoadError;
+        public event Action<string> LoadError;
 
         public List<Language> AvailableLanguages { get; private set; }
         public Language Current { get; private set; }
+        public string Path { get; private set; }
         public string this[string key]
         {
-            get { return ""; }
+            get
+            {
+                if (Current.Name != "null")
+                    return file[Current.Name][key] != null ? file[Current.Name][key].ToString() : key;
+
+                return key;
+            }
         }
 
-        string path;
+        INI.INIFile file;
 
-        public Manager(string folderPath)
+        public Manager(string path)
         {
-            path = folderPath;
+            Path = path;
+            AvailableLanguages = new List<Language>();
+            Current = new Language() { Name = "null" };
         }
 
-        public void Load(Language lang)
+        public void Load()
         {
-            Current = AvailableLanguages.Single(l => l.Code == lang.Code);
-            if (Current.Load())
-                LanguageChanged?.Invoke(this);
-            else
-                LoadError?.Invoke();
+            if(!File.Exists(Path))
+            {
+                LoadError?.Invoke($"File {Path} not found!");
+                return;
+            }
+
+            file = new INI.INIFile(Path, "=", ";", System.Text.Encoding.UTF8);
+
+            foreach (var item in file.Sections)
+                if(!item.IsRoot)
+                {
+                    Language l = new Language() { Name = item.Name, Code = item["code"].ToString() };
+                    AvailableLanguages.Add(l);
+                }
+        }
+        public void SetCurrent(string code)
+        {
+            Current = AvailableLanguages.Single(l => l.Code == code);
+            LanguageChanged?.Invoke(this);
         }
     }
 }
